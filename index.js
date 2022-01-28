676aa9c4a9d8906ff0f6bde422202175e98f5c5a
Gabriel Mejia <gabrielm29929@gmail.com>
2022-01-27 21:28:22 -0800

    added more answers

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ It scampers off down a small hole, just large enough for you to crawl through.
 Do you follow it, or continue on your path?`)
     if (secondAnswer === 'follow it') {
         let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
-        if(secondAnswer === 'stay') {
-            let secondAnswer = window.prompt('You live happily amongs the cats for the rest of your days.')
-        } else if (secondAnswer === 'spread the word') {
-            let secondAnswer = window.alert('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
+        if(thirdAnswer === 'stay') {
+            let thirdAnswer = window.prompt('You live happily amongs the cats for the rest of your days.')
+        } else if (fourthAnswer === 'spread the word') {
+            let fourthAnswer = window.alert('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
         }
-        if (secondAnswer === 'Continue') {
-            let secondAnswer = window.prompt('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?')
-            if(secondAnswer === 'ladder') {
-                let secondAnswer = window.prompt('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
+        if (thirdAnswer === 'Continue') {
+            let thirdAnswer = window.prompt('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?')
+            if(fourthAnswer === 'ladder') {
+                let fourthAnswer = window.prompt('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
             } else if (secondAnswer === 'staircase') {
                 let secondAnswer = window.alert('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
             }
